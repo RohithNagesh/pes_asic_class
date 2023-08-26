0afe2d286938e51ca7e5fa83b658227daa4a4168
ROHITH D N <103078929+RohithNagesh@users.noreply.github.com>
2023-08-26 14:34:33 +0530

    Update yosys.sh

--- yosys.sh
+++ yosys.sh
@@ -1,8 +1,18 @@
 git clone https://github.com/YosysHQ/yosys.git
 cd yosys
-sudo apt install make
-sudo apt-get update
-sudo apt-get install build-essential clang bison flex  libreadline-dev gawk tcl-dev libffi-dev git  graphviz xdot pkg-config python3 libboost-system-dev libboost-python-dev libboost-filesystem-dev zlib1g-dev
+sudo apt install make -y
+sudo apt-get update -y
+sudo apt-get install build-essential -y
+sudo apt-get install clang -y
+sudo apt-get install bison -y
+sudo apt-get install flex  -y
+sudo apt-get install libreadline-dev -y
+sudo apt-get install gawk -y
+sudo apt-get install tcl-dev -y
+sudo apt-get install libffi-dev -y 
+sudo apt-get install git -y
+sudo apt-get install graphviz -y 
+sudo apt-get install xdot pkg-config python3 libboost-system-dev libboost-python-dev libboost-filesystem-dev zlib1g-dev -y
 make config-gcc
 make
 sudo make install
